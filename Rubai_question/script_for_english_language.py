ee394bdbd12538635941cc40033f2c77212642c8
minhaz-18 <minhaz.mahmud.18@gmail.com>
2023-05-22 06:16:24 -0700

    update

---
+++ Rubai_question/script_for_english_language.py
@@ -0,0 +1,13 @@
+# Add your Python code here
+def run_program():
+    # Your program logic goes here
+    output = "English language Question Paper"
+    return output
+
+# if __name__ == "__main__":
+#     output = run_program()
+#     print(output)  # This will print the output to the console
+
+a = run_program()
+print(a)
+
